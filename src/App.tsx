1d34946fa52e832c5480b52be2b8490d9df7a226
m.shimono <m.shimono@fabeee.co.jp>
2023-01-26 23:51:53 +0900

    changed App.tsx for refactaring

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,20 @@
 import { useState } from "react";
 import { invoke } from "@tauri-apps/api/tauri";
-import {writeText} from '@tauri-apps/api/clipboard'; 
+import { writeText } from '@tauri-apps/api/clipboard'; 
 import "./App.css";
 
-function App() {
-  const [StartStampMsg, StartStamp] = useState("");
-  const [EndStampMsg, EndStamp] = useState("");
-  
-  async function end_stamp() {
-      EndStamp(await invoke("end_stamp"));
- }
+const App = () => {
+  const [message, setMessage] = useState("");
 
-  async function start_stamp() {
-      StartStamp(await invoke("start_stamp"));
- }
-  async function writeStartStampClipboard(){
-    // start stamp to clipboard
-    await writeText(StartStampMsg)
-  }
-  async function writeEndStampClipboard(){
-    // end stamp to clipboard
-    await writeText(EndStampMsg)
-  }
-  
+  const handleClickInvokeStamp = async (key: "end_stamp" | "start_stamp") => {
+    setMessage(await invoke(key));
+  };
+
+  const handleClickClipboard = async () => {
+    //  write to clipboard
+    await writeText(message);
+  };
+
   return (
     <div className="container">
       <h1>Welcome to ChronoStamp</h1>
@@ -31,25 +23,27 @@ function App() {
 
       <div className="row">
         <div>
-          <button type="button" onClick={() => start_stamp()}>
+          <button
+            type="button"
+            onClick={() => handleClickInvokeStamp("start_stamp")}
+          >
             出勤
           </button>
-          <button type="button" onClick={() => end_stamp()}>
+          <button type="button" onClick={() => handleClickInvokeStamp("end_stamp")}>
             退勤
           </button>
         </div>
       </div>
-      <p>{EndStampMsg}</p>
-      <p>{StartStampMsg}</p>
-          <button type="button" onClick={() => writeStartStampClipboard()}>
-            Start Clip 
-          </button>
-          <button type="button" onClick={() => writeEndStampClipboard()}>
-            End   Clip 
-          </button>
+
+      <p>{message}</p>
+
+      <button type="button" onClick={handleClickClipboard}>
+        Clipboardにコピーする
+      </button>
     </div>
   );
-}
+};
+
 
 export default App;
 
